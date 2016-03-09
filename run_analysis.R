7830ed084df69cdb09474bbb596e1451fd3a12ef
nishantatgit <nk.muzaffarpur@gmail.com>
2016-03-09 21:59:44 +0530

    Added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,31 @@
 rm(list = ls())
-print("reading features...")
+
+#reading features
 features <- read.table("features.txt",header = FALSE, stringsAsFactors = FALSE)
 features <- features[[2]]
 features <- gsub("\\(|\\)", "", features)
 features <- gsub("\\-","_", features)
 
-print("reading activity labels..")
+#reading activity labels
 act_label <- read.table("activity_labels.txt",header = FALSE, stringsAsFactors = FALSE)
 
 
 setwd("train")
 
-print("reading training set..")
+#reading training set
 x_train <- read.table("x_train.txt", header = FALSE)
 y_train <- read.table("y_train.txt", header = FALSE)
 subject_train <- read.table("subject_train.txt", header = FALSE)
 
 
 setwd("../test")
-print("reading test set...")
+
+#reading test set
 x_test <- read.table("x_test.txt",header = FALSE)
 y_test <- read.table("y_test.txt", header = FALSE)
 subject_test <- read.table("subject_test.txt", header = FALSE)
 
-setwd("..")
+#setting column names for dataframes
 colnames(x_test) <- features
 colnames(x_train) <- features
 colnames(y_test) <- c("id")
@@ -48,14 +50,16 @@ x_train$activity <- y_train[[2]]
 x_train <- cbind(x_train,subject_train)
 x_test$activity <- y_test[[2]]
 x_test <- cbind(x_test,subject_test)
-print("merging training and test sets...")
+
+#merging training and test sets
 tidy_data <- rbind(x_train,x_test)
 
-print("done...")
+
 library(data.table)
 tidy_data2 <- data.table(tidy_data)
 
 tidy_data2<- tidy_data2[, lapply(.SD, mean), by=c("subject", "activity")]
+tidy_data2 <-  tidy_data2[order(subject,activity),]
 
-
-
+# writing data to the outputfile
+write.table(tidy_data2,file = "tidy_data.txt" , row.names = FALSE)
